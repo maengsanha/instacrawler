e813344383539cac8cd1dace051d05c86ceafeb0
joshua-dev <msh01175@gmail.com>
2020-02-29 23:30:52 +0900

    Test Succedded: Scraping from Instagram

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# instagramscrapper
-KMU-CS Capstone Project: Instagram Scrapper Server
+![Thumbnail](https://img.icons8.com/cotton/2x/instagram-new.png)
+
+# INSTAGRAM SCRAPER
+
+[![LICENSE](https://img.shields.io/badge/license-Apache%202-blue)](https://github.com/joshua-dev/instagramscrapper/blob/master/LICENSE)
+[![go version](https://img.shields.io/badge/go-1.14-00ADD8)](https://go.dev)
+
+---
+
+### [LICENSE](https://github.com/joshua-dev/instagramscrapper/blob/master/LICENSE)
---
+++ scraper/scraper.go
@@ -0,0 +1,31 @@
+package scraper
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const baseURL = "https://instagram.com/explore/tags/"
+
+// Scrape scrapes results from searches on terms in Instagram.
+func Scrape(term string) {
+	var targetURL string = baseURL + term + "/"
+
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	result := string(bytes)
+	fmt.Println(result)
+}
